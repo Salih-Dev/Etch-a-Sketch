57f4563ddaefd79978686d773ac7d168eddd7547
salih <180684400+Salih-Dev@users.noreply.github.com>
2026-08-17 21:58:27 +0530

    commit for sleep

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const container = document.querySelector("#container");
 
-for(let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-square");
-    container.appendChild(div);
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "blue";
-    });
+function createSquares(number) {
+    for (let i = 0; i < 16 * 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-square");
+        container.appendChild(div);
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "blue";
+        });
+    };
 };
+
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let response = prompt("Enter number of squares per side(1-100): ");
+
+    if (response > 100) {
+        alert("Please enter an intiger between 1 and 100")
+    }
+
+    createSquares(response);
+});
